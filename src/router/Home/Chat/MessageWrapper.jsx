bce81431c14f260bfbf8884af1b5abe9d4cd2e1e
hquan26122003 <hquan26122003@gmail.com>
2023-06-06 21:45:34 +0700

    d

--- src/router/Home/Chat/MessageWrapper.jsx
+++ src/router/Home/Chat/MessageWrapper.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';;
 import { useLoaderData } from 'react-router-dom';
-import { messagesSelector } from '../../../app/messageSlice';
 import useChat from '../../../firebase/chatsController';
 import ChatItem from './ChatItem';
 
